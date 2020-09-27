e6043ef5685b4610aac723c1aff81e0972d354ab
baduker <baduker@protonmail.ch>
2020-09-27 18:41:56 +0200

    Some fixes.

--- whtasupaws.go
+++ whtasupaws.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/microcosm-cc/bluemonday"
-	"time"
 )
 
 func removeHTMLTags(item string) string {
@@ -24,19 +23,13 @@ func parseNews() AWSNewsData {
 }
 
 func show(news AWSNewsData, wrap int) {
-	today := time.Now().Format("01-02-2006")
-
 	for index, i := range news.Items {
 		date := i.Item.AdditionalFields.PostDateTime.Format("01-02-2006")
-		if date == today {
-			headline := i.Item.AdditionalFields.Headline
-			postBody := wordWrap(removeHTMLTags(i.Item.AdditionalFields.PostBody), wrap)
-			link := fmt.Sprintf("https://aws.amazon.com%s", i.Item.AdditionalFields.HeadlineURL)
-			fmt.Printf("%d. %s\nPublished: %s\n%s\n\n", index+1, headline, date, link)
-			fmt.Printf("%s\n\n", postBody)
-		} else {
-			fmt.Println("There are no news for today.")
-		}
+		headline := i.Item.AdditionalFields.Headline
+		postBody := wordWrap(removeHTMLTags(i.Item.AdditionalFields.PostBody), wrap)
+		link := fmt.Sprintf("https://aws.amazon.com%s", i.Item.AdditionalFields.HeadlineURL)
+		fmt.Printf("%d. %s\nPublished: %s\n%s\n\n", index+1, headline, date, link)
+		fmt.Printf("%s\n\n", postBody)
 	}
 }
 
